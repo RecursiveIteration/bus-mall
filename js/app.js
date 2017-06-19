8ffe0d12645b77159bc259b204403df3e3170693
RecursiveIteration <James.C.Woodson@gmail.com>
2017-06-19 14:22:28 -0700

    Added initial css/html, created Product class and created jpgs for all images

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
 'use strict';
 
-var dummyVariable = 'dummy';
-console.log(dummyVariable);
+var products = [
+  'bag',
+  'banana',
+  'bathroom',
+  'boots',
+  'breakfast',
+  'bubblegum',
+  'chair',
+  'cthulhu',
+  'dog-duck',
+  'dragon',
+  'pen',
+  'pet-sweep',
+  'scissors',
+  'shark',
+  'sweep',
+  'tauntaun',
+  'unicorn',
+  'usb',
+  'water-can',
+  'wine-glass'
+];
+
+function Product (name) {
+  this.name = name;
+  this.imageURL = '../images' + name + '.jpg';
+  this.numberOfClicks = 0;
+  this.numberOfShows = 0;
+}
